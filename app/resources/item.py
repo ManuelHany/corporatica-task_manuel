400e9b84cdde45dcb5a0231ffbb01614683ead8a
Manuel <manuel.hany@umbrasys.com>
2024-08-19 16:53:58 +0300

    migrate to SQLAlchemy stage 2 - adding many to many relationships new table 'tags' and new endpoints

--- app/resources/item.py
+++ app/resources/item.py
@@ -21,7 +21,10 @@ class Item(MethodView):
 
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Deleting an item is not implemented.")
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted"}
+
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
@@ -43,7 +46,7 @@ class Item(MethodView):
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
 
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
